74d479c371f5e3c39fd57478fe04b3a091456cef
trzl <brownty47@gmail.com>
2018-07-23 21:22:01 +0000

    Updating build script
    - adding push command to hub.docker

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@ docker system prune --volumes && \
 mkdir services/react-nginx/build && \
 cp -r services/react/build/client services/react-nginx/build && \
 docker build -t trzl/portfolio:shutterbugg-proxy.1 -f ./services/reverseproxy/Dockerfile ./services/reverseproxy && \
-# docker push trzl/portfolio:shutterbuggg-proxy.1 && \
+docker push trzl/portfolio:shutterbugg-proxy.1 && \
 docker build -t trzl/portfolio:shutterbugg-react.1 -f ./services/react-nginx/build.Dockerfile ./services/react-nginx && \
-# docker push trzl/portfolio:shutterbuggg-react.1 && \
+docker push trzl/portfolio:shutterbugg-react.1 && \
 docker-compose down && \
 docker system prune --volumes && \
 sudo rm -rf services/react-nginx/build && \
